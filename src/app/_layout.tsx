1a30bf376efce34e200d52ee9867e828f037543a
Mwiti Peter <mwitipet@gmail.com>
2025-03-18 22:30:54 +0300

    secon native wind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,10 @@
-import{ Stack } from 'expo-router';
+import { Stack } from "expo-router";
 import "../../global.css";
 
 export default function RootLayout() {
   return (
     <Stack>
-        <Stack.Screen name="index" options={{ title: "Meditations"}} />
+      <Stack.Screen name="index" options={{ title: "Meditations" }} />
     </Stack>
-  )
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Text } from "react-native";
 
 export default function HomeScreen() {
-  return <Text className="font-bold text-4xl p-10 bg-red-500 color-blue-500">Home screen</Text>;
+  return <Text className="font-extrabold text-center">Home screen</Text>;
 }
